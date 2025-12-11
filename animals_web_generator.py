ebe1cabaf9299561db9370c1b40d2cfda9ea0ecc
v3gas90b <v3gas90b@gmail.com>
2025-12-11 10:08:08 +0100

    Updated animals_web_generator.py - added function that loads a html file data, updated print_data function in treating the FileNotFoundError.

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,9 +1,15 @@
 import json
 
-def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+def load_json_data(json_file_path):
+    """ Loads a JSON file """
+    with open(json_file_path, "r") as handle:
+        return json.load(handle)
+
+
+def load_html_data(html_file_path):
+    """ Loads a html file """
+    with open(html_file_path, "r") as file_object:
+        return file_object.read()
 
 
 def print_data():
@@ -12,22 +18,23 @@ def print_data():
     location and type of every animal.
     If one of these fields doesn’t exist, it doesn’t print it.
     """
-    animals_data = load_data('animals_data.json')
-    for animal in animals_data:
-        animal_name = animal.get("name", "")
-        animal_diet = animal["characteristics"].get("diet", "")
-        animal_location = animal.get("locations", "")
-        animal_type = animal["characteristics"].get("type", "")
+    try:
+        animals_data = load_json_data('animals_data.json')
+        for animal in animals_data:
+            animal_name = animal.get("name", "")
+            animal_diet = animal["characteristics"].get("diet", "")
+            animal_location = animal.get("locations", "")
+            animal_type = animal["characteristics"].get("type", "")
 
-        if animal_name != "":
-            print(f"Name: {animal_name}")
-        if animal_diet != "":
-            print(f"Diet: {animal_diet}")
-        if animal_location != "":
-            print("Location: ", end="")
-            print(', '.join(map(str, animal_location)))
-        if animal_type != "":
-            print(f"Type: {animal_type}")
-        print()
-
-print_data()
+            if animal_name != "":
+                print(f"Name: {animal_name}")
+            if animal_diet != "":
+                print(f"Diet: {animal_diet}")
+            if animal_location != "":
+                print("Location: ", end="")
+                print(', '.join(map(str, animal_location)))
+            if animal_type != "":
+                print(f"Type: {animal_type}")
+            print()
+    except FileNotFoundError:
+        print("File not found!")
